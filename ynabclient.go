2685b249f7ed05ba1c3d1829abd4b9e688b159ae
crab-apple <andreu.bastardas@gmail.com>
2024-08-09 23:48:27 +0200

    Separate main class and client into different files

---
+++ ynabclient.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func readAccessToken() (string, error) {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("unable to read access token: %w", err)
+	}
+	f := filepath.Join(homedir, ".ynab", "access_token")
+	contents, err := os.ReadFile(f)
+	if err != nil {
+		return "", fmt.Errorf("unable to read access token: %w", err)
+	}
+	return strings.TrimSpace(string(contents)), nil
+}
+
+func readBudgets(token string) (string, error) {
+
+	client := http.Client{}
+
+	req, err := http.NewRequest("GET", "https://api.ynab.com/v1/budgets", nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error while reading budgets,  %w", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error while reading budgets,  %w", err)
+	}
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("error while reading budgets, status code %d: %s", resp.StatusCode, body)
+	}
+
+	return string(body), nil
+}
