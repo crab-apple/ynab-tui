01fc4d1de3f65069fb3e6f1078fc5efd24208b27
crab-apple <andreu.bastardas@gmail.com>
2024-09-29 13:21:36 +0200

    Use TestEnv in the test that was still not using it

--- cmd/ynabtui_test.go
+++ cmd/ynabtui_test.go
@@ -2,33 +2,20 @@ package main
 
 import (
 	"github.com/stretchr/testify/require"
-	"io"
-	"sync"
 	"testing"
-	"time"
-	"ynabtui/internal/app"
 	"ynabtui/internal/ynabmodel"
 	"ynabtui/test"
 )
 
 func TestQQuitsProgram(t *testing.T) {
 
-	output := io.Discard
+	env := test.NewTestEnv(t)
 
-	inputReader, inputWriter := io.Pipe()
+	env.Run()
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
+	env.Type('q')
 
-	go func() {
-		app.RunApp(inputReader, output, test.NewFakeYnab().Api(), AppFilesFake{})
-		wg.Done()
-	}()
-
-	_, err := inputWriter.Write([]byte("q"))
-	require.NoError(t, err)
-
-	require.False(t, waitTimeout(&wg, 100*time.Millisecond))
+	env.GetOutput()
 }
 
 func TestDisplaysTransactions(t *testing.T) {
@@ -49,27 +36,4 @@ func TestDisplaysTransactions(t *testing.T) {
 
 	require.Contains(t, visible, "Last minute groceries")
 	require.Contains(t, visible, "Chewing gum")
-}
-
-type AppFilesFake struct {
-}
-
-func (AppFilesFake) GetLogWriter() (io.Writer, func(), error) {
-	return io.Discard, func() {}, nil
-}
-
-// waitTimeout waits for the waitgroup for the specified max timeout.
-// Returns true if waiting timed out.
-func waitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
-	c := make(chan struct{})
-	go func() {
-		defer close(c)
-		wg.Wait()
-	}()
-	select {
-	case <-c:
-		return false // completed normally
-	case <-time.After(timeout):
-		return true // timed out
-	}
 }
