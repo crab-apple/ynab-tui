34cbb09f4e3f1f772a5d75a408648be43d5e9d98
crab-apple <andreu.bastardas@gmail.com>
2024-12-19 13:13:29 +0100

    Delete tests that try to assert on the terminal output
    I don't like the direction in which this is going. It's probably better to
    write most of the UI logic separately from BubbleTea, and keep the BubbleTea
    part as thin as possible.

--- test/app_test.go
+++ test/app_test.go
@@ -1,39 +1,17 @@
 package test
 
 import (
-	"bytes"
 	"github.com/charmbracelet/x/exp/teatest"
 	"testing"
 	"time"
-	"ynabtui/internal/ynabmodel"
+	"ynabtui/internal/app"
 )
 
 func TestQQuitsProgram(t *testing.T) {
 
-	env := NewTestEnv(t)
-
-	tm := env.Start()
+	tm := teatest.NewTestModel(t, app.InitialModel(NewFakeYnab().Api()), teatest.WithInitialTermSize(120, 30))
 
 	tm.Type("q")
 
 	tm.WaitFinished(t, teatest.WithFinalTimeout(500*time.Millisecond))
-}
-
-func TestDisplaysTransactions(t *testing.T) {
-
-	env := NewTestEnv(t)
-
-	env.Ynab.SetTransactions([]ynabmodel.Transaction{
-		MakeTransaction(&AccChecking, &CatGroceries, "2020-01-01", 12340, "Last minute groceries"),
-		MakeTransaction(&AccCash, &CatGroceries, "2020-01-02", 3500, "Chewing gum"),
-		MakeTransaction(&AccChecking, &CatRent, "2020-01-02", 1000000, ""),
-	})
-
-	env.Start()
-
-	env.AssertOutput(func(bts []byte) bool {
-		return bytes.Contains(bts, []byte("Last minute groceries")) && bytes.Contains(bts, []byte("Chewing gum"))
-	})
-
-	env.Quit()
 }
