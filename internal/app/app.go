640940d414d6663070c20dc69a8438c65272d621
crab-apple <andreu.bastardas@gmail.com>
2025-03-22 10:36:18 +0100

    Make the configurator unaware of the "UI" object

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,14 +5,16 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"io"
 	"os"
-	"ynabtui/app/app/ui"
+	app2 "ynabtui/app/app"
 	"ynabtui/app/driven_ports"
 	tea2 "ynabtui/internal/ui/tea"
 )
 
 func RunApp(input io.Reader, output io.Writer, forCommunicatingWithYnab driven_ports.ForCommunicatingWithYnab) {
 
-	p := tea.NewProgram(tea2.InitialModel(ui.NewUI(forCommunicatingWithYnab)), tea.WithInput(input), tea.WithOutput(output), tea.WithAltScreen())
+	var app = app2.NewApp(forCommunicatingWithYnab)
+
+	p := tea.NewProgram(tea2.InitialModel(app.ForDisplayingTheScreen()), tea.WithInput(input), tea.WithOutput(output), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
