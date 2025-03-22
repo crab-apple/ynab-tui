1a50302129f52e62f342116eaf17827eb97e2ba9
crab-apple <andreu.bastardas@gmail.com>
2025-03-22 12:38:55 +0100

    Fix linting

--- internal/ui/test/app_test.go
+++ internal/ui/test/app_test.go
@@ -4,14 +4,12 @@ import (
 	"github.com/charmbracelet/x/exp/teatest"
 	"testing"
 	"time"
-	"ynabtui/app/driving_ports"
 	"ynabtui/internal/ui/tea"
 )
 
 func TestQQuitsProgram(t *testing.T) {
 
-	var forDisplayingTheScreen driving_ports.ForDisplayingTheScreen
-	forDisplayingTheScreen = FakeScreenSupplier{}
+	forDisplayingTheScreen := FakeScreenSupplier{}
 
 	tm := teatest.NewTestModel(t, tea.InitialModel(forDisplayingTheScreen), teatest.WithInitialTermSize(120, 30))
 
